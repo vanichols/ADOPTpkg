47499540bb66dd8290121c650a473379e43fadb0
vanichols <virginia.nichols@gmail.com>
2025-11-05 16:37:46 +0100

    cleaning up ridge and dot figs

--- R/make_dot_plot.R
+++ R/make_dot_plot.R
@@ -22,7 +22,7 @@ make_dot_plot <- function(data = opat_example,
     c(
       "Crop value",
       "Costs",
-      "Time/management complexity",
+      "Time/management\ncomplexity",
       "Immediate usability",
       "Environmental impact",
       "User health and safety"
@@ -37,7 +37,7 @@ make_dot_plot <- function(data = opat_example,
 
 cat_range <- 7.5
 
-  plot_data <-
+  plot_data1 <-
     data |>
     rename(rating_numeric = rating_1to5) |>
     mutate(metric = c(metric_names6, metric_names6),
@@ -50,54 +50,85 @@ cat_range <- 7.5
                 "many-to-many") |>
     #--calculate utility
     group_by(title, metricF, weights) |>
-    summarise(utility = weighted.mean(value_bin, w = score)) |>
+    summarise(utility = weighted.mean(value_bin, w = score),
+              utility_sd = (sd(score))^2/(5))
+
+  #--calculate differences in mean utility
+  plot_data2 <-
+    plot_data1 |>
+    select(-utility_sd) |>
       #--calculate change from baseline to new approach
       pivot_wider(names_from = title,
                   values_from = utility) |>
       rename (base = 3, new = 4) |>
-      mutate(diff_pct = (new - base)/base*100,
-             diff_cat = case_when(
-               (diff_pct <= -cat_range - cat_range*2) ~ "Really bad",
-               (diff_pct > -cat_range - cat_range*2) & (diff_pct <= -cat_range) ~ "Bad",
-               (diff_pct > -cat_range) & (diff_pct <= cat_range) ~ "Neutral",
-               (diff_pct > cat_range) & (diff_pct <= cat_range + cat_range*2) ~ "Good",
-               diff_pct > cat_range + cat_range*2 ~ "Really good")
-      )
+      mutate(diff_pct = (new - base)/base*100,diff_pct_abs = round(abs(diff_pct), -1),
+             diff_pct_label = ifelse(diff_pct < 0, paste0("-", diff_pct_abs, "%"),
+                                     paste0("+", diff_pct_abs, "%")))
+
+  #--calculate uncertainty in differences
+  plot_data3 <-
+    plot_data1 |>
+    select(-utility) |>
+    #--calculate change from baseline to new approach
+    pivot_wider(names_from = title,
+                values_from = utility_sd) |>
+    rename (base = 3, new = 4) |>
+    mutate(diff_sd = sqrt(base + new)) |>
+    select(metricF, diff_sd)
 
   #--experimenting
+  #--be cool to have size relative to certainty?
 
-   plot_data |>
-     mutate(diff_pct_abs = abs(diff_pct),
-            dummy = "hi") |>
+   plot_data2 |>
+     left_join(plot_data3) |>
+     mutate(dummy = "hi") |>
      ggplot(aes(x = metricF,
                 y = dummy)) +
-     geom_point(
-       aes(fill = diff_pct,
-           size = weights),
-       pch = 21) +
-     scale_fill_gradient2() +
-     scale_size_continuous(range = c(6, 20), guide = "none") +
+     # geom_point(
+     #   aes(fill = diff_pct,
+     #       size = weights),
+     #   pch = 21) +
+     geom_label(aes(y = dummy,
+                   label = diff_pct_label,
+                   size = diff_sd,
+                   fill = diff_pct),
+                show.legend = F) +
+     scale_fill_gradient2(mid = "white",
+                          #high = "#08519c",
+                          high = "#06386b",
+                          low = "#8d2e01",
+                          ) +
+     scale_size_continuous(range = c(1, 5), guide = "none") +
+     # scale_y_continuous(limits = c(-0.5, 1.5),
+     #                    expand = expansion(0.001)) +
      coord_flip() +
      labs(
       x = NULL,
       y = NULL,
-      fill = "Change in performance"
+      title = "Change in performance with new strategy"
     ) +
+     guides(fill = guide_legend(title.position = "top")) +
     # Theme
     theme_minimal() +
-    theme(
-      legend.position = "top",
-      legend.title = element_text(face = "bold"),
-      legend.background = element_rect(fill='transparent'),
-      legend.box.background = element_rect(fill='transparent'),
-      #panel.grid.major.x = element_blank(),
-      panel.grid.minor = element_blank(),
-      axis.text.x = element_blank(),
-      #axis.text.y = element_blank(),
-      # strip.text = element_text(face = "bold", size = rel(1.1)),
-      plot.title = element_text(hjust = 0.5, face = "bold"),
-      plot.subtitle = element_text(hjust = 0.5)
-    )
+     theme(
+       legend.title = element_blank(),
+       legend.position = "top",
+       legend.justification = "center",
+       legend.box = "horizontal",
+       legend.key = element_blank(),
+       legend.box.margin = margin(),
+       legend.margin = margin(),
+       plot.title.position = "plot",
+       plot.caption.position = "plot",
+       plot.caption = element_text(hjust = 0),
+       legend.location = "plot",
+       #--get rid of minor gridlines
+       panel.grid = element_blank(),
+       #--ratings text
+       axis.text.x = element_blank(),
+       plot.title = element_text(hjust = 0.5, face = "bold"),
+       plot.subtitle = element_text(hjust = 0.5)
+     )
 
 
    }
--- R/make_ridge_plots.R
+++ R/make_ridge_plots.R
@@ -12,34 +12,6 @@
 make_ridge_plots <- function(data = opat_example,
                              betas = opat_betas) {
 
-  metric_colors <- c(
-    # Environmental fate
-    "Soil persistence (DT50 soil)"         = "#ffffcc",
-    "Water persistence (DT50 water)"       = "#c2e699",
-    "Surface water transfer (Kfoc)"        = "#78c679",
-    "Groundwater transfer (GUS)"           = "#31a354",
-    "Aquatic biome transfer (BCF)"         = "#006837",
-    # Ecotoxicity (terrestrial)
-    "Birds (acute oral)"                   = "#feedde",
-    "Earthworms (acute soil)"              = "#fdbe85",
-    "Honeybees (acute oral/contact/other)" = "#fd8d3c",
-    "Mammals (acute oral)"                 = "#d94701",
-    # Ecotoxicity (aquatic)
-    "Algae (acute aqueous)"                = "#eff3ff",
-    "Aquatic invertebrates (acute aq.)"    = "#bdd7e7",
-    "Aquatic invertebrates (chronic aq.)"  = "#6baed6",
-    "Fish (acute aqueous)"                 = "#3182bd",
-    "Fish (chronic aqueous)"               = "#08519c",
-    # Human health
-    "Mammals (acute dermal)"               = "#feebe2",
-    "Mammals (acute inhalation)"           = "#fcc5c0",
-    "Humans (carcinogenicity)"             = "#fa9fb5",
-    "Humans (cholinesterase inhibition)"   = "#f768a1",
-    "Humans (neurotoxicity)"               = "#c51b8a",
-    "Humans (reprotoxicity)"               = "#7a0177"
-  )
-
-
   metric_colors6 <- c(
     "Crop value" =  "#c2e699",
     "Costs" = "#fd8d3c",
@@ -53,7 +25,7 @@ make_ridge_plots <- function(data = opat_example,
     c(
       "Crop value",
       "Costs",
-      "Time/management complexity",
+      "Time/management\ncomplexity",
       "Immediate usability",
       "Environmental impact",
       "User health and safety"
@@ -69,30 +41,31 @@ make_ridge_plots <- function(data = opat_example,
 
   plot_data <-
     data |>
-    rename(rating_numeric = rating_1to5) |>
-    mutate(metric = c(metric_names6, metric_names6),
+    dplyr::rename(rating_numeric = rating_1to5) |>
+    dplyr::mutate(metric = c(metric_names6, metric_names6),
            metricF = factor(metric, levels = rev(metric_names6))) |>
     #--join with confidence bins
-    left_join(betas,
+    dplyr::left_join(betas,
               relationship =
                 "many-to-many") |>
-    select(title, metricF, value_bin, score)
+    dplyr::select(title, metricF, value_bin, score)
 
-  #--experimenting
+  #--ridge plot
 
    plot_data |>
-     mutate(score = as.integer(score)) |>
-     uncount(score) |>
+     dplyr::mutate(score = as.integer(score)) |>
+     tidyr::uncount(score) |>
      ggplot(aes(x = value_bin,
                 y = metricF)) +
-     geom_density_ridges2(
+     ggridges::geom_density_ridges2(
        aes(fill = title),
        alpha = 0.6,
        bandwidth = 0.5,
        scale = 0.9 #--height of distributions
        ) +
-     scale_fill_manual(values = c("#fdbe85", "#08519c"),
-                       guide = guide_legend(ncol = 1)) +
+     scale_fill_manual(values = c("gray10", "#ffffcc"),
+                       #values = c("#fdbe85", "#08519c"),
+                       guide = guide_legend(ncol = 2)) +
      scale_x_continuous(
        breaks = c(1, 2, 3, 4, 5),
        labels = c("Unacceptable",
@@ -102,20 +75,29 @@ make_ridge_plots <- function(data = opat_example,
                   "Highly acceptable")
      ) +
     labs(
-      x = "Performance",
+      title = "Performance",
       y = NULL,
-      fill = NULL
+      fill = NULL,
+      x = NULL
     ) +
     # Theme
     theme_minimal() +
     theme(
+      legend.title = element_blank(),
       legend.position = "top",
-      legend.title = element_text(face = "bold"),
-      #panel.grid.major.x = element_blank(),
+      legend.justification = "center",
+      legend.box = "horizontal",
+      legend.key = element_blank(),
+      legend.box.margin = margin(),
+      legend.margin = margin(),
+      plot.title.position = "plot",
+      plot.caption.position = "plot",
+      plot.caption = element_text(hjust = 0),
+      legend.location = "plot",
+      #--get rid of minor gridlines
       panel.grid.minor = element_blank(),
+      #--ratings text
       axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1),
-      # axis.text.y = element_blank(),
-      # strip.text = element_text(face = "bold", size = rel(1.1)),
       plot.title = element_text(hjust = 0.5, face = "bold"),
       plot.subtitle = element_text(hjust = 0.5)
     )
@@ -124,4 +106,4 @@ make_ridge_plots <- function(data = opat_example,
    }
 
 # #--testing function
-#make_rose_plot_pair()
+#make_ridge_plots()
