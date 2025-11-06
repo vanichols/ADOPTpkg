f2bc5625c06cfa68399003fe43e03da51df1264a
vanichols <virginia.nichols@gmail.com>
2025-11-06 13:31:23 +0100

    Add weight column to ex questionaire

--- data-raw/example.R
+++ data-raw/example.R
@@ -14,7 +14,8 @@ d1 <- readxl::read_excel("data-raw/Example questionnaire.xlsx",
 d2 <-
   d1 %>%
   tidyr::fill(title, pesticide_load) |>
-  dplyr::mutate(pesticide_load = as.numeric(pesticide_load)) |>
+  dplyr::mutate(pesticide_load = as.numeric(pesticide_load),
+                weight = as.numeric(weight)) |>
   dplyr::select(-notes)
 
 opat_example <- d2
