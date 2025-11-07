dcf0321b5d1dfcda35d0594d6f4d4e335c9b456d
vanichols <virginia.nichols@gmail.com>
2025-11-07 10:42:37 +0100

    add new fxn to make ridge plots for ind strategy

--- R/make_ridge_plots.R
+++ R/make_ridge_plots.R
@@ -5,6 +5,7 @@
 #' @returns A plot
 #' @import ggplot2
 #' @import stringr
+#' @import forcats
 #' @import ggridges
 #' @export
 
@@ -12,16 +13,21 @@
 make_ridge_plots <- function(data = opat_example,
                              betas = opat_betas) {
 
-  metric_colors6 <- c(
+  metric_colors <- c(
     "Crop value" =  "#c2e699",
-    "Costs" = "#fd8d3c",
-    "Time/management complexity" = "#f768a1",
+    "Costs" = "#ffffcc",
+    "Time/management\ncomplexity" = "#fa9fb5",
     "Immediate usability" = "#fdbe85",
-    "Environmental impact" = "#7a0177",
+    "Environmental impact" = "#c51b8a",
     "User health and safety" = "#6baed6"
   )
 
-  metric_names6 <-
+  metric_names <-
+    data |>
+    pull(metric) |>
+    unique()
+
+  metric_names_nice <-
     c(
       "Crop value",
       "Costs",
@@ -31,41 +37,61 @@ make_ridge_plots <- function(data = opat_example,
       "User health and safety"
     )
 
-
   #--get names of approaches
-  strategy_names <-
+  strategy_name <-
     data |>
-    dplyr::select(title) |>
-    dplyr::distinct()
+    dplyr::pull(title) |>
+    unique()
 
+  if(length(strategy_name) > 0){
+
+    m1 <- paste("Hi! The dataset you provided has more than one strategy.\nThe plots will reflect the first one listed:",
+                strategy_name[1])
+    warning(m1)
+  }
 
   plot_data <-
     data |>
+    dplyr::filter(title == strategy_name[1]) |>
     dplyr::rename(rating_numeric = rating_1to5) |>
-    dplyr::mutate(metric = c(metric_names6, metric_names6),
-           metricF = factor(metric, levels = rev(metric_names6))) |>
+    #--make metric into a factor
+    mutate(
+      metric2 = case_when(
+        metric == metric_names[1] ~ metric_names_nice[1],
+        metric == metric_names[2] ~ metric_names_nice[2],
+        metric == metric_names[3] ~ metric_names_nice[3],
+        metric == metric_names[4] ~ metric_names_nice[4],
+        metric == metric_names[5] ~ metric_names_nice[5],
+        metric == metric_names[6] ~ metric_names_nice[6]),
+      metricF = factor(metric2, levels = (metric_names_nice))) |>
     #--join with confidence bins
     dplyr::left_join(betas,
               relationship =
                 "many-to-many") |>
-    dplyr::select(title, metricF, value_bin, score)
+    #--make some things for the figure
+    arrange(metricF) |>
+    dplyr::mutate(metric_label = paste0(metricF, " (", round(weight, 2), "%)"),
+                  metricF = as.factor(metric_label),
+                  metricF = forcats::fct_inorder(metricF)) |>
+    dplyr::mutate(score = as.integer(score)) |>
+    dplyr::select(title, metricF, weight, value_bin, score)
 
   #--ridge plot
 
    plot_data |>
-     dplyr::mutate(score = as.integer(score)) |>
      tidyr::uncount(score) |>
-     ggplot(aes(x = value_bin,
-                y = metricF)) +
-     ggridges::geom_density_ridges2(
-       aes(fill = title),
-       alpha = 0.6,
-       bandwidth = 0.5,
-       scale = 0.9 #--height of distributions
-       ) +
-     scale_fill_manual(values = c("gray10", "#ffffcc"),
-                       #values = c("#fdbe85", "#08519c"),
-                       guide = guide_legend(ncol = 2)) +
+     ggplot() +
+     geom_density(
+       (aes(x = value_bin,
+            fill = metricF)),
+       bw = 0.5,
+       show.legend = F
+     ) +
+     geom_col(data = plot_data,
+              aes(value_bin, score/100),
+              alpha = 0.5) +
+   facet_wrap(~metricF, labeller = label_wrap_gen(width = 20)) +
+     scale_fill_manual(values = unname(metric_colors)) +
      scale_x_continuous(
        breaks = c(1, 2, 3, 4, 5),
        labels = c("Unacceptable",
@@ -74,8 +100,13 @@ make_ridge_plots <- function(data = opat_example,
                   "Acceptable",
                   "Highly acceptable")
      ) +
+     scale_y_continuous(
+       breaks = c(0, .2, .4, .6, .8, 1),
+       limits = c(0, 1),
+       labels = scales::label_percent(),
+     ) +
     labs(
-      title = "Performance",
+      title = paste(strategy_name[1]),
       y = NULL,
       fill = NULL,
       x = NULL
