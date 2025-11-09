b63ab9c948078365fd5375ba8d02c02d075aa473
vanichols <virginia.nichols@gmail.com>
2025-11-09 17:58:02 +0100

    add Noes palette for reference

---
+++ R/palette.R
@@ -0,0 +1,29 @@
+#### Color palette #############################################################
+
+metric_colors <- c(
+  # Environmental fate
+    "Soil persistence (DT50 soil)"         = "#ffffcc",
+    "Water persistence (DT50 water)"       = "#c2e699",
+    "Surface water transfer (Kfoc)"        = "#78c679",
+    "Groundwater transfer (GUS)"           = "#31a354",
+    "Aquatic biome transfer (BCF)"         = "#006837",
+  # Ecotoxicity (terrestrial)
+    "Birds (acute oral)"                   = "#feedde",
+    "Earthworms (acute soil)"              = "#fdbe85",
+    "Honeybees (acute oral/contact/other)" = "#fd8d3c",
+    "Mammals (acute oral)"                 = "#d94701",
+  # Ecotoxicity (aquatic)
+    "Algae (acute aqueous)"                = "#eff3ff",
+    "Aquatic invertebrates (acute aq.)"    = "#bdd7e7",
+    "Aquatic invertebrates (chronic aq.)"  = "#6baed6",
+    "Fish (acute aqueous)"                 = "#3182bd",
+    "Fish (chronic aqueous)"               = "#08519c",
+  # Human health
+    "Mammals (acute dermal)"               = "#feebe2",
+    "Mammals (acute inhalation)"           = "#fcc5c0",
+    "Humans (carcinogenicity)"             = "#fa9fb5",
+    "Humans (cholinesterase inhibition)"   = "#f768a1",
+    "Humans (neurotoxicity)"               = "#c51b8a",
+    "Humans (reprotoxicity)"               = "#7a0177"
+)
+
